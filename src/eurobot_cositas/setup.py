27f12f92de7fd2ab48f78991e35ad91521c85e54
Samuel Vera <sfveragarcia@gmail.com>
2026-02-17 16:07:41 +0100

    Add installation for Gazebo world and RViz config

--- src/eurobot_cositas/setup.py
+++ src/eurobot_cositas/setup.py
@@ -14,13 +14,24 @@ setup(
         ('share/' + package_name + '/launch', glob('launch/*.py')),
         # ↓ Esto copia los archivos del rosbag al install
         ('share/' + package_name + '/rosbag2', glob('rosbag2/*')),
+                # Instalar mundo de Gazebo
+        (os.path.join('share', package_name, 'world'),
+            glob('world/*')),
+
+        # Instalar configuracion de RViz
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.rviz')),
+
+            #Templates
+        (os.path.join('share', package_name, 'templates'), glob('templates/*.png')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
         'console_scripts': [
-            # ↓ Esto registra el ejecutable subcriptor
             'subcriptor = eurobot_cositas.subcriptor:main',
+            'aruco_detector_cositas = eurobot_cositas.aruco_detector_cositas:main',
+            'vision_navigation = eurobot_cositas.vision_navigation:main'
         ],
     },
 )
